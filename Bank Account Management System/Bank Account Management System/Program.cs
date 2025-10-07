559120d4edb1e8554a5dfd44b10870dcd8e495e5
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2025-10-07 13:01:42 +0530

    Create Abstract Base Class Account

--- Bank Account Management System/Bank Account Management System/Program.cs
+++ Bank Account Management System/Bank Account Management System/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿abstract class Account
+{
+    string AccountNumber;
+    decimal Balance;
+
+    public Account (string accountNumber, decimal balance)
+    {
+        this.AccountNumber = accountNumber;
+        this.Balance = balance;
+    }
+
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited {amount:C} successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Deposit amount must be positive!");
+        }
+    }
+
+    public virtual void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn {amount:C} successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid amount or insufficient funds!");
+        }
+    }
+
+    public abstract void ShowInfo();
+}
